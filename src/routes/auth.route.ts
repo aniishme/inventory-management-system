0d06cd989d8429f0d8add583e4f048538129c870
Anish Sharma <sharmaanish023@gmail.com>
2022-08-25 20:51:47 +0545

    zod to validate user registeration input  data

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,31 @@
 import { Router } from "express";
 import { createUser, loginUser } from "../controllers/auth.controller";
+import { z } from "zod";
+import { validate } from "../utils/zod.validate";
 const authRouter = Router();
 
-authRouter.post("/register", createUser);
+const dataSchema = z.object({
+  body: z.object({
+    fullname: z.string({
+      required_error: "Fullname is required",
+    }),
+    username: z
+      .string({
+        required_error: "Username is required",
+      })
+      .min(3, "Username must be at least 3 characters long"),
+    password: z
+      .string({
+        required_error: "Password is required",
+      })
+      .min(6, "Password must be at least 6 characters long"),
+    role: z.string({
+      required_error: "Role is required",
+    }),
+  }),
+});
+
+authRouter.post("/register", validate(dataSchema), createUser);
 authRouter.post("/login", loginUser);
 
 export default authRouter;
