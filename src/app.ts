1a13d680d96798e31bb02c711cfbd508cce662ba
Anish Sharma <sharmaanish023@gmail.com>
2022-09-05 08:58:58 +0545

    FIX #3 cors origin, credentials fixed

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,12 @@ import {
 } from "./middlewares/authorization.middleware";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
