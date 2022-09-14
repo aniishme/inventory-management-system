32f590368cb2c6f287c5e862a0bf1c18e115114d
Anish Sharma <sharmaanish023@gmail.com>
2022-09-14 18:08:06 +0545

    cookie samesite set to none and secure to true

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -60,7 +60,13 @@ export const loginUser = async (req: Request, res: Response) => {
     res.cookie(
       "session",
       { token },
-      { httpOnly: true, path: "/", maxAge: ONE_WEEK_IN_MS }
+      {
+        httpOnly: true,
+        path: "/",
+        maxAge: ONE_WEEK_IN_MS,
+        sameSite: "none",
+        secure: true,
+      }
     );
     return res.status(200).json({ message: "Cookie Set" });
   } catch (error: any) {
